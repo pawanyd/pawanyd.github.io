d17a7a0960d1c472003cbb07c07e727c29548a05
Pawan Kumar <pawanyd2012@gmail.com>
2025-02-14 00:51:50 +0530

    implement tailwind css package

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './_layouts/**/*.html',
+      './_includes/**/*.html',
+      './_posts/*.md',
+      './*.html',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+};
+  
