ac83a7cbcb7bd6cc495482eb2a2665fef045807d
Pawan Kumar <pawanyd2012@gmail.com>
2026-01-26 20:14:28 +0530

    minify done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,17 @@ module.exports = {
     content: [
       './_layouts/**/*.html',
       './_includes/**/*.html',
-      './_posts/*.md',
-      './blog/*.md',
+      './_posts/**/*.md',
+      './blog/**/*.html',
+      './projects/**/*.html',
       './*.html',
-      './*.md'
+      './*.md',
+      './assets/js/**/*.js'
+    ],
+    safelist: [
+      'bg-yellow-200',
+      'font-semibold',
+      'line-clamp-2'
     ],
     theme: {
       extend: {},
